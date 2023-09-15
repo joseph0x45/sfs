aa6dd2b7cef8cc3d5eb3dc482571d16bdd574dd1
TheWisePigeon <zozozozeph@gmail.com>
2023-09-15 16:31:04 +0000

    minor updates

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
-	dir := "./static"
+  port := os.Getenv("PORT")
+  // port = ":8080"
 
-	fileServer := http.FileServer(http.Dir(dir))
-
-	http.Handle("/", http.StripPrefix("/", fileServer))
-
-	err := http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+      http.ServeFile(w, r, "./static/test.html")
+		}
+	})
+  fmt.Println("Server listening on ", port)
+  err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil)
 	if err != nil {
 		panic(err)
 	}
 }
-
